ca3a1587da817d80378fd75a10bb7d7e51836715
Mihai Pop <mihai.m.m.pop@gmail.com>
2019-06-10 20:52:59 +0300

    Added assignment 1

---
+++ src/main/java/ro/utcn/sd/mpop/virtualclassroom/VirtualClassroomApplication.java
@@ -0,0 +1,13 @@
+package ro.utcn.sd.mpop.virtualclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualClassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualClassroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/utcn/sd/mpop/virtualclassroom/VirtualClassroomApplicationTests.java
@@ -0,0 +1,16 @@
+package ro.utcn.sd.mpop.virtualclassroom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualClassroomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
